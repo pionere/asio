8f5c64753b4111abb8ca42aa9aee2b66a8060d70
Christopher Kohlhoff <chris@kohlhoff.com>
2020-03-09 21:09:40 +1100

    Add execution::outstanding_work property.

--- asio/include/asio.hpp
+++ asio/include/asio.hpp
@@ -56,6 +56,7 @@
 #include "asio/execution/blocking.hpp"
 #include "asio/execution/executor.hpp"
 #include "asio/execution/invocable_archetype.hpp"
+#include "asio/execution/outstanding_work.hpp"
 #include "asio/execution_context.hpp"
 #include "asio/executor.hpp"
 #include "asio/executor_work_guard.hpp"

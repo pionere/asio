0e8330c7d30a3514b930c3a48075fa5c0f512a89
Christopher Kohlhoff <chris@kohlhoff.com>
2020-07-07 12:13:47 +1000

    Replace identical helper structs with void_type.

--- asio/include/asio/bind_executor.hpp
+++ asio/include/asio/bind_executor.hpp
@@ -29,12 +29,6 @@
 namespace asio {
 namespace detail {
 
-template <typename T>
-struct executor_binder_check
-{
-  typedef void type;
-};
-
 // Helper to automatically define nested typedef result_type.
 
 template <typename T, typename = void>
@@ -46,7 +40,7 @@ protected:
 
 template <typename T>
 struct executor_binder_result_type<T,
-  typename executor_binder_check<typename T::result_type>::type>
+  typename void_type<typename T::result_type>::type>
 {
   typedef typename T::result_type result_type;
 protected:
@@ -108,7 +102,7 @@ struct executor_binder_argument_type {};
 
 template <typename T>
 struct executor_binder_argument_type<T,
-  typename executor_binder_check<typename T::argument_type>::type>
+  typename void_type<typename T::argument_type>::type>
 {
   typedef typename T::argument_type argument_type;
 };
@@ -133,7 +127,7 @@ struct executor_binder_argument_types {};
 
 template <typename T>
 struct executor_binder_argument_types<T,
-  typename executor_binder_check<typename T::first_argument_type>::type>
+  typename void_type<typename T::first_argument_type>::type>
 {
   typedef typename T::first_argument_type first_argument_type;
   typedef typename T::second_argument_type second_argument_type;
@@ -199,7 +193,7 @@ struct executor_binder_result_of0
 
 template <typename T>
 struct executor_binder_result_of0<T,
-  typename executor_binder_check<typename result_of<T()>::type>::type>
+  typename void_type<typename result_of<T()>::type>::type>
 {
   typedef typename result_of<T()>::type type;
 };

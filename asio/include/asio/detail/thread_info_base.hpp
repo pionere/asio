b7ca468d241f39dbaac6577176e01c6c9aef8c90
Christopher Kohlhoff <chris@kohlhoff.com>
2021-07-17 09:59:02 +1000

    Clean up memory recycling to ensure the cache size is used consistently.

--- asio/include/asio/detail/thread_info_base.hpp
+++ asio/include/asio/detail/thread_info_base.hpp
@@ -25,6 +25,10 @@
 namespace asio {
 namespace detail {
 
+#ifndef ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+# define ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE 2
+#endif // ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+
 class thread_info_base
   : private noncopyable
 {
@@ -33,12 +37,9 @@ public:
   {
     enum
     {
+      cache_size = ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE,
       begin_mem_index = 0,
-#ifdef ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
-      end_mem_index = ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
-#else // ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
-      end_mem_index = 2
-#endif // ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+      end_mem_index = cache_size
     };
   };
 
@@ -46,13 +47,9 @@ public:
   {
     enum
     {
+      cache_size = ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE,
       begin_mem_index = default_tag::end_mem_index,
-#ifdef ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
-      end_mem_index =
-        begin_mem_index + ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
-#else // ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
-      end_mem_index = begin_mem_index + 2
-#endif // ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+      end_mem_index = begin_mem_index + cache_size
     };
   };
 
@@ -60,8 +57,9 @@ public:
   {
     enum
     {
+      cache_size = ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE,
       begin_mem_index = awaitable_frame_tag::end_mem_index,
-      end_mem_index = begin_mem_index + 1
+      end_mem_index = begin_mem_index + cache_size
     };
   };
 

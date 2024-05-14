60b26779b883d13e7701dd0955a05f297454e7e4
pionere <pionere@freemail.hu>
2024-05-14 11:30:14 +0200

    ASIO_HAS_STD_IOSTREAM_MOVE

--- asio/include/asio/basic_socket_iostream.hpp
+++ asio/include/asio/basic_socket_iostream.hpp
@@ -167,8 +167,6 @@ public:
     this->setf(std::ios_base::unitbuf);
   }
 
-#if defined(ASIO_HAS_STD_IOSTREAM_MOVE) \
-  || defined(GENERATING_DOCUMENTATION)
   /// Move-construct a basic_socket_iostream from another.
   basic_socket_iostream(basic_socket_iostream&& other)
     : detail::socket_iostream_base<
@@ -190,8 +188,6 @@ public:
         WaitTraits ASIO_SVC_TARG1>::operator=(std::move(other));
     return *this;
   }
-#endif // defined(ASIO_HAS_STD_IOSTREAM_MOVE)
-       //   || defined(GENERATING_DOCUMENTATION)
 
 #if defined(GENERATING_DOCUMENTATION)
   /// Establish a connection to an endpoint corresponding to a resolver query.

605ad7bdf31ae2119ab80c7430b9dc8dd79a8ebc
pionere <pionere@freemail.hu>
2024-05-14 10:58:15 +0200

    ASIO_HAS_ALIAS_TEMPLATES

--- asio/include/asio/associated_allocator.hpp
+++ asio/include/asio/associated_allocator.hpp
@@ -116,13 +116,9 @@ get_associated_allocator(const T& t, const Allocator& a) noexcept
   return associated_allocator<T, Allocator>::get(t, a);
 }
 
-#if defined(ASIO_HAS_ALIAS_TEMPLATES)
-
-template <typename T, typename Allocator = std::allocator<void> >
+template <typename T, typename Allocator = std::allocator<void>>
 using associated_allocator_t
   = typename associated_allocator<T, Allocator>::type;
-
-#endif // defined(ASIO_HAS_ALIAS_TEMPLATES)
 
 } // namespace asio
 

BRevzin/asio-1

Mark posix descriptor classes' move constructors as noexcept.

asio/include/asio/posix/basic_descriptor.hpp
@@ -168,7 +168,7 @@ public:
    * constructed using the @c basic_descriptor(const executor_type&)
    * constructor.
    */
-  basic_descriptor(basic_descriptor&& other)
+  basic_descriptor(basic_descriptor&& other) ASIO_NOEXCEPT
     : impl_(std::move(other.impl_))
   {
   }

asio/include/asio/posix/basic_stream_descriptor.hpp
@@ -144,7 +144,7 @@ public:
    * constructed using the @c basic_stream_descriptor(const executor_type&)
    * constructor.
    */
-  basic_stream_descriptor(basic_stream_descriptor&& other)
+  basic_stream_descriptor(basic_stream_descriptor&& other) ASIO_NOEXCEPT
     : descriptor(std::move(other))
   {
   }

pionere/asio

asio/include/asio/posix/basic_descriptor.hpp
@@ -99,7 +99,7 @@ public:
    * @note Following the move, the moved-from object is in the same state as if
    * constructed using the @c basic_descriptor(io_context&) constructor.
    */
-  basic_descriptor(basic_descriptor&& other)
+  basic_descriptor(basic_descriptor&& other) noexcept
     : basic_io_object<DescriptorService>(
         static_cast<basic_descriptor&&>(other))
   {

asio/include/asio/posix/basic_stream_descriptor.hpp
@@ -99,7 +99,7 @@ public:
    * @note Following the move, the moved-from object is in the same state as if
    * constructed using the @c basic_stream_descriptor(io_context&) constructor.
    */
-  basic_stream_descriptor(basic_stream_descriptor&& other)
+  basic_stream_descriptor(basic_stream_descriptor&& other) noexcept
     : basic_descriptor<StreamDescriptorService>(
         static_cast<basic_stream_descriptor&&>(other))
   {
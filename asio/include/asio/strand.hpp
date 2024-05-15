BRevzin/asio-1

Fix strand<> converting construction and assigment.

asio/include/asio/strand.hpp
@@ -108,7 +108,7 @@ public:
    */
   template <class OtherExecutor>
   strand(strand<OtherExecutor>&& other) ASIO_NOEXCEPT
-    : executor_(ASIO_MOVE_CAST(OtherExecutor)(other)),
+    : executor_(ASIO_MOVE_CAST(OtherExecutor)(other.executor_)),
       impl_(ASIO_MOVE_CAST(implementation_type)(other.impl_))
   {
   }
@@ -116,7 +116,7 @@ public:
   /// Move assignment operator.
   strand& operator=(strand&& other) ASIO_NOEXCEPT
   {
-    executor_ = ASIO_MOVE_CAST(Executor)(other);
+    executor_ = ASIO_MOVE_CAST(Executor)(other.executor_);
     impl_ = ASIO_MOVE_CAST(implementation_type)(other.impl_);
     return *this;
   }
@@ -127,10 +127,9 @@ public:
    * convertible to @c Executor.
    */
   template <class OtherExecutor>
-  strand& operator=(
-      const strand<OtherExecutor>&& other) ASIO_NOEXCEPT
+  strand& operator=(strand<OtherExecutor>&& other) ASIO_NOEXCEPT
   {
-    executor_ = ASIO_MOVE_CAST(OtherExecutor)(other);
+    executor_ = ASIO_MOVE_CAST(OtherExecutor)(other.executor_);
     impl_ = ASIO_MOVE_CAST(implementation_type)(other.impl_);
     return *this;
   }
@@ -264,7 +263,9 @@ public:
     return a.impl_ != b.impl_;
   }
 
+#if defined(GENERATING_DOCUMENTATION)
 private:
+#endif // defined(GENERATING_DOCUMENTATION)
   Executor executor_;
   typedef detail::strand_executor_service::implementation_type
     implementation_type;

pionere/asio

asio/include/asio/strand.hpp
@@ -107,7 +107,7 @@ public:
    */
   template <class OtherExecutor>
   strand(strand<OtherExecutor>&& other) noexcept
-    : executor_(static_cast<OtherExecutor&&>(other)),
+    : executor_(static_cast<OtherExecutor&&>(other.executor_)),
       impl_(static_cast<implementation_type&&>(other.impl_))
   {
   }
@@ -115,7 +115,7 @@ public:
   /// Move assignment operator.
   strand& operator=(strand&& other) noexcept
   {
-    executor_ = static_cast<Executor&&>(other);
+    executor_ = static_cast<Executor&&>(other.executor_);
     impl_ = static_cast<implementation_type&&>(other.impl_);
     return *this;
   }
@@ -126,9 +126,9 @@ public:
    * convertible to @c Executor.
    */
   template <class OtherExecutor>
-  strand& operator=(const strand<OtherExecutor>&& other) noexcept
+  strand& operator=(strand<OtherExecutor>&& other) noexcept
   {
-    executor_ = static_cast<OtherExecutor&&>(other);
+    executor_ = static_cast<OtherExecutor&&>(other.executor_);
     impl_ = static_cast<implementation_type&&>(other.impl_);
     return *this;
   }
@@ -261,7 +261,9 @@ public:
     return a.impl_ != b.impl_;
   }
 
+#if defined(GENERATING_DOCUMENTATION)
 private:
+#endif // defined(GENERATING_DOCUMENTATION)
   Executor executor_;
   typedef detail::strand_executor_service::implementation_type
     implementation_type;